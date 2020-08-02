45de2c7cef5dcf8b6d6fabe2746e1a935e5d236a
magz99 <magzb.99@gmail.com>
2020-08-02 16:12:03 -0400

    Updates scanner to create a master JSON file containing mappings

--- app-scanner/scanner.js
+++ app-scanner/scanner.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 const axeCore = require('axe-core');
 const fs = require('fs');
 const readline = require('readline');
+const { create } = require('domain');
 
 let siteName = '';
 let urlFile = '';
@@ -10,9 +11,22 @@ let scanMobile = false;
 let resultFolder = ''; // Default, will be timestamped
 let resultPrefix = 'scan_'; // Default
 let urlCounter = 0;
+let createMasterJSONOnly = false;
 
 let browser = null;
 let page = null;
+
+let masterObj = {
+  urlScanned: siteName,
+  totalViolations: 0,
+  scannedPages: [],
+  topViolations: {
+    critical: 0,
+    serious: 0,
+    moderate: 0,
+    minor: 0,
+  },
+};
 
 const appArgumentsDesc = `
   Usage: node scanner.js -urlFile <path> -siteName <name> [-resultFolder <path>] [-level <value>] [-mobile]
@@ -41,11 +55,11 @@ const launchPuppeteer = () => {
   return puppeteer.launch({ headless: false });
 };
 
-const newBrowserPage = browser => {
+const newBrowserPage = (browser) => {
   return browser.newPage();
 };
 
-const injectAxeLib = page => {
+const injectAxeLib = (page) => {
   return page.evaluateHandle(`
   // Inject axe source code
   ${axeCore.source}
@@ -64,7 +78,7 @@ const writeResultsToFile = (jsonResult, fName) => {
   fs.writeFileSync(fName, data);
 };
 
-const runScanOnPage = async urlFromFile => {
+const runScanOnPage = async (urlFromFile) => {
   await goToPage(page, urlFromFile);
 
   // Inject and run axe-core
@@ -81,8 +95,84 @@ const runScanOnPage = async urlFromFile => {
   await handle.dispose();
 };
 
-const cleanup = async browser => {
+const cleanup = async (browser) => {
   await browser.close();
+  createMasterJSON();
+};
+
+const readScanDir = async (scanFolderPath) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(scanFolderPath, (err, items) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(items);
+      }
+    });
+  });
+};
+
+const mergeJSONData = (fileArray, scanFolderPath) => {
+  return fileArray.map((fName) => {
+    // Read the JSON file
+    return new Promise((resolve, reject) => {
+      fs.readFile(scanFolderPath + fName, 'utf8', (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          const jsonResult = JSON.parse(data);
+          const violations = jsonResult.violations;
+          let totalViolations = 0;
+          let topViolations = {
+            critical: 0,
+            serious: 0,
+            moderate: 0,
+            minor: 0,
+          };
+
+          violations.forEach((violation) => {
+            totalViolations += violation.nodes.length;
+            topViolations[violation.impact] += violation.nodes.length;
+          });
+
+          // Updates the masterObject
+          masterObj.totalViolations += totalViolations;
+          masterObj.scannedPages.push({
+            url: jsonResult.url,
+            filename: fName,
+          });
+
+          masterObj.topViolations = {
+            critical: masterObj.topViolations.critical + topViolations.critical,
+            serious: masterObj.topViolations.serious + topViolations.serious,
+            moderate: masterObj.topViolations.moderate + topViolations.moderate,
+            minor: masterObj.topViolations.minor + topViolations.minor,
+          };
+
+          resolve(true);
+        }
+      });
+    });
+  });
+
+  //return true;
+  // Loop through all the files, and create a JSON array of results
+};
+
+const createMasterJSON = async (scanFolderPath, resultFileName) => {
+  masterObj.urlScanned = resultFileName;
+  console.log('Creating master JSON file.');
+  const fileArray = await readScanDir(scanFolderPath);
+
+  const temp2 = mergeJSONData(fileArray, scanFolderPath);
+
+  Promise.all(temp2).then(() => {
+    // Write the masterJSON file
+    fs.writeFileSync(
+      scanFolderPath + resultFileName + '.json',
+      JSON.stringify(masterObj)
+    );
+  });
 };
 
 const launchScanner = async () => {
@@ -90,7 +180,7 @@ const launchScanner = async () => {
   try {
     await setUpEnvironment();
     const readStream = fs.createReadStream(urlFile, {
-      encoding: 'utf-8'
+      encoding: 'utf-8',
     });
     const readLine = readline.createInterface(readStream);
 
@@ -118,6 +208,13 @@ const launchScanner = async () => {
 
     try {
       switch (val) {
+        case '-masterOnly':
+          createMasterJSONOnly = true;
+          siteName = 'magz-test';
+          resultFolder = `./scans/magz-test/2020-08-02T18-30-20.460Z/`;
+          urlFile =
+            '/home/magz/workspace/crawls/magz-test/magz-test_2020-08-02T01-19-56.619916.txt';
+          break;
         case '-siteName':
           siteName = getValue();
           resultFolder = `./scans/${siteName}/`;
@@ -155,14 +252,21 @@ const launchScanner = async () => {
     validArgs = false;
   }
   if (validArgs) {
-    fs.promises
-      .mkdir(resultFolder + '/', { recursive: true })
-      .then(async () => {
-        launchScanner();
-      })
-      .catch(err => {
-        throw err;
-      });
+    if (createMasterJSONOnly) {
+      createMasterJSON(resultFolder, 'magz1');
+    } else {
+      // create a folder that is timestamped
+      const tstamp = new Date().toISOString().replace(/:/g, '-');
+      fs.promises
+        .mkdir(resultFolder + '/' + tstamp, { recursive: true })
+        .then(async () => {
+          resultFolder = resultFolder + '/' + tstamp + '/';
+          launchScanner();
+        })
+        .catch((err) => {
+          throw err;
+        });
+    }
   } else {
     console.log(appArgumentsDesc);
   }
